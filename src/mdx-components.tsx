4383b0d668892bb52a0e2f9dc8d256f4ba21a9b8
heynzar <heynzar@gmail.com>
2025-12-02 18:01:06 +0200

    🍀 refactor: fix alt issue & clean MDX img typing

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,14 +1,21 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import type { ComponentProps } from "react";
 import ContentNavigation from "@/components/content-navigation";
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 
-// use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ContentNavigation,
-    img: (props) => <ImageZoom {...(props as any)} />,
+    img: (props: ComponentProps<"img">) => (
+      <ImageZoom
+        alt={props.alt ?? "wiki nzar dev visual"}
+        width={props.width as number | undefined}
+        height={props.height as number | undefined}
+        zoomInProps={props}
+      />
+    ),
     ...components,
   };
 }
