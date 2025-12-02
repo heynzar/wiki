76c757bfbac6d1d955a585e1afd8c5db05fca71c
heynzar <heynzar@gmail.com>
2025-12-02 17:38:52 +0200

    🍀 feat: add image zoom component

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,12 +1,14 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import ContentNavigation from "@/components/content-navigation";
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ContentNavigation,
+    img: (props) => <ImageZoom {...(props as any)} />,
     ...components,
   };
 }
