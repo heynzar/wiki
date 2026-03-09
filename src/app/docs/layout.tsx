9348b0c5931e746b5d36873769e3b9da61d8356f
heynzar <heynzar@gmail.com>
2026-03-09 09:02:47 +0000

    fix: global layout

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,10 +1,23 @@
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
-import { baseOptions } from "@/lib/layout.shared";
 import { source } from "@/lib/source";
+import Image from "next/image";
 
 export default function Layout({ children }: LayoutProps<"/docs">) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout
+      tree={source.pageTree}
+      nav={{
+        title: (
+          <Image
+            alt="Wiki Nzar Dev Logo"
+            src="/logo.svg"
+            width={40}
+            height={20}
+            className="h-5 w-auto dark:grayscale-100 dark:invert-100"
+          />
+        ),
+      }}
+    >
       {children}
     </DocsLayout>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,24 @@
 import "@/app/global.css";
 import { RootProvider } from "fumadocs-ui/provider/next";
-import { Inter } from "next/font/google";
+import { Inter, Instrument_Serif } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
+  variable: "--font-inter",
+});
+const instrument_serif = Instrument_Serif({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-instrument-serif",
 });
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={`${inter.className}`} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${inter.variable} ${instrument_serif.variable}`}
+      suppressHydrationWarning
+    >
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
